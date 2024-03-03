ea3faf20e4964efc506f092051543a2d1e442f36
Mayush99 <mayushkumar01@gmail.com>
2024-03-03 11:47:26 +0530

    changed services page

--- shravana-app/app/(tabs)/Services.tsx
+++ shravana-app/app/(tabs)/Services.tsx
@@ -1,12 +1,34 @@
-import { StyleSheet } from 'react-native';
-
-import EditScreenInfo from '@/components/EditScreenInfo';
-import { Text, View } from '@/components/Themed';
+import React from 'react';
+import { StyleSheet, View, TouchableOpacity, Text, Image } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { Link } from 'expo-router';
 
 export default function Services() {
   return (
     <View style={styles.container}>
-      <Text>Services</Text>
+      <Text style={styles.title}>Services</Text>
+      <View style={styles.gridContainer}>
+        <TouchableOpacity style={styles.button}>
+          <Image source={require('../../images/healthcare.webp')} style={styles.smallImage} />
+          <Text style={styles.buttonText}>Healthcare</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button}>
+          <Image source={require('../../images/retail.png')} style={styles.smallImage} />
+          <Text style={styles.buttonText}>Retail Shopping</Text>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.gridContainer}>
+        <Link href='/services/Delivery'>
+          <TouchableOpacity style={styles.button}>
+            <Image source={require('../../images/scooter.png')} style={styles.smallImage} />
+            <Text style={styles.buttonText}>Delivery/Pickup</Text>
+          </TouchableOpacity>
+        </Link>
+        <TouchableOpacity style={styles.button}>
+          <Image source={require('../../images/homem.png')} style={styles.smallImage} />
+          <Text style={styles.buttonText}>Home Maintenance</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
@@ -16,14 +38,35 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+    padding: 16,
   },
   title: {
     fontSize: 20,
     fontWeight: 'bold',
+    marginBottom: 16,
   },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
+  gridContainer: {
+    flexDirection: 'row',
+    marginVertical: 8,
+  },
+  button: {
+    flex: 1,
+    marginHorizontal: 8,
+    backgroundColor: 'lightblue',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 8,
+    padding: 18,
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 15,
+    marginTop: 8,
+    fontWeight: 'bold',
+  },
+  smallImage: {
+    width: 100, // Set the width to the desired size
+    height: 100, // Set the height to the desired size
+    resizeMode: 'contain',
   },
 });
