1bfe6c516da70f05ff1da56d27cf4f73ded94ff8
Mayush99 <mayushkumar01@gmail.com>
2024-03-03 11:46:40 +0530

    added services page

---
+++ shravana-app/app/services/Delivery.tsx
@@ -0,0 +1,28 @@
+import { StyleSheet } from 'react-native';
+
+import EditScreenInfo from '@/components/EditScreenInfo';
+import { Text, View } from '@/components/Themed';
+
+const DeliveryScreen = () => {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.title}>Delivery/Pickup Screen</Text>
+        <Text>This is a dummy delivery screen.</Text>
+      </View>
+    );
+  };
+  
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    title: {
+      fontSize: 20,
+      fontWeight: 'bold',
+      marginBottom: 16,
+    },
+  });
+
+export default DeliveryScreen;
