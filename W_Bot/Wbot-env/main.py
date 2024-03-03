1624b663ee4cee1a347be61bad84afb9fb9d8287
Yash1038 <yashaswilnpasumarthy@gmail.com>
2024-03-03 15:16:34 +0530

    Updated logic

--- W_Bot/Wbot-env/main.py
+++ W_Bot/Wbot-env/main.py
@@ -20,10 +20,19 @@ def bot():
 
 	# User Query
 	if user_msg == "hi":
-		response.message("Hello, How can I help you?")
+		response.message("Welcome to Shravana Bot. Please reply with any of the following options: \n 1. Home Maintenance \n 2. Delivery \n 3. Groceries \n 4. Medical Services")
 
-	elif user_msg == "hello":
-		response.message("Welcome to Shravana Bot")
+	elif user_msg == "1":
+		response.message("Please select any of the following options: \n a. Electrician \n b. Plumber \n c. Carpenter \n d. Painter")
+
+	elif user_msg == "2":
+		response.message("Please select any of the following options: \n a. Food Delivery \n b. Parcel Delivery")
+
+	elif user_msg == "3":
+		response.message("Please select any of the following options: \n a. Vegetables \n b. Fruits \n c. Groceries")
+
+	elif user_msg == "4":
+		response.message("Please select any of the following options: \n a. Doctor Booking \n b. Medicine")
 
 	else :
 		response.message("I am still learning, I will get back to you soon")
